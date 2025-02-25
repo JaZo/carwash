e9fd86dbf3d9b6178ad89835bb434901c33b8188
Shift <shift@laravelshift.com>
2025-02-25 20:00:40 +0000

    Define test classes as `final`

--- tests/CommandRegistrationTest.php
+++ tests/CommandRegistrationTest.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class CommandRegistrationTest extends TestCase
+final class CommandRegistrationTest extends TestCase
 {
     use DatabaseTransactions;
     const CARWASH_COMMANDS = [
